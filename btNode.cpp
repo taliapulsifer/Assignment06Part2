59ede05db1c6bd0607228d0b5616f26415123daa
Talia Pulsifer <taliapulsifer@outlook.com>
2023-11-19 13:02:15 -0600

    Add project files.

---
+++ btNode.cpp
@@ -0,0 +1,40 @@
+#include "btNode.h"
+
+// write definition for bst_insert here
+
+
+// write definition for bst_remove here
+
+
+// write definition for bst_remove_max here
+
+
+void portToArrayInOrder(btNode* bst_root, int* portArray)
+{
+   if (bst_root == 0) return;
+   int portIndex = 0;
+   portToArrayInOrderAux(bst_root, portArray, portIndex);
+}
+
+void portToArrayInOrderAux(btNode* bst_root, int* portArray, int& portIndex)
+{
+   if (bst_root == 0) return;
+   portToArrayInOrderAux(bst_root->left, portArray, portIndex);
+   portArray[portIndex++] = bst_root->data;
+   portToArrayInOrderAux(bst_root->right, portArray, portIndex);
+}
+
+void tree_clear(btNode*& root)
+{
+   if (root == 0) return;
+   tree_clear(root->left);
+   tree_clear(root->right);
+   delete root;
+   root = 0;
+}
+
+int bst_size(btNode* bst_root)
+{
+   if (bst_root == 0) return 0;
+   return 1 + bst_size(bst_root->left) + bst_size(bst_root->right);
+}
